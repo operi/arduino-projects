14d8665d623c8affda1c2dae9335b18dface7ca9
Octavio <octaperi@gmail.com>
2020-07-26 02:00:59 -0300

    Use NodeMCU's pin naming

--- NodeMcuGarage/NodeMcuGarage.ino
+++ NodeMcuGarage/NodeMcuGarage.ino
@@ -8,8 +8,8 @@
 ESP8266WiFiMulti wifiMulti;
 ESP8266WebServer server(80);
 
-const int pinPump = 4;
-const int pinLight = 5;
+const int pinPump = D1;
+const int pinLight = D2;
 
 void handleRoot();
 void handlePump();
