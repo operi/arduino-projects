dfb1d929b9d9c21c95e56f4a2ba2a6aa68af0b9c
Octavio <octaperi@gmail.com>
2020-08-08 14:44:48 -0300

    Remove comments

--- NodeMcuGarage/NodeMcuGarage.ino
+++ NodeMcuGarage/NodeMcuGarage.ino
@@ -12,8 +12,8 @@ ESP8266WebServer server(80);
 const int pinPump = 5;
 const int pinLight = 4;
 
-String getContentType(String filename); // convert the file extension to the MIME type
-bool handleFileRead(String path);       // send the right file to the client (if it exists)
+String getContentType(String filename);
+bool handleFileRead(String path);
 
 void setup(void) {
   Serial.begin(115200);
@@ -48,9 +48,9 @@ void setup(void) {
 
   server.on("/pump", HTTP_POST, handlePump);
   server.on("/light", HTTP_POST, handleLight);
-  server.onNotFound([]() {                              // If the client requests any URI
-    if (!handleFileRead(server.uri()))                  // send it if it exists
-      server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
+  server.onNotFound([]() {
+    if (!handleFileRead(server.uri()))
+      server.send(404, "text/plain", "404: Not Found");
   });
 
   server.begin();
@@ -70,22 +70,22 @@ String getContentType(String filename){
   return "text/plain";
 }
 
-bool handleFileRead(String path){  // send the right file to the client (if it exists)
+bool handleFileRead(String path){
   Serial.println("handleFileRead: " + path);
-  if(path.endsWith("/")) path += "index.html";           // If a folder is requested, send the index file
-  String contentType = getContentType(path);             // Get the MIME type
+  if(path.endsWith("/")) path += "index.html";
+  String contentType = getContentType(path);
   String pathWithGz = path + ".gz";
-  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){  // If the file exists, either as a compressed archive, or normal
-    if(SPIFFS.exists(pathWithGz))                          // If there's a compressed version available
-      path += ".gz";                                         // Use the compressed version
-    File file = SPIFFS.open(path, "r");                    // Open the file
-    size_t sent = server.streamFile(file, contentType);    // Send it to the client
-    file.close();                                          // Close the file again
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+    if(SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
     Serial.println(String("\tSent file: ") + path);
     return true;
   }
   Serial.println(String("\tFile Not Found: ") + path);
-  return false;                                          // If the file doesn't exist, return false
+  return false;
 }
 
 void handlePump() {
