44f631b5e8d0d001b97ea95965e1445f302f010b
Octavio <octaperi@gmail.com>
2020-08-08 14:29:58 -0300

    Add filesystem handling
    Use files for statics instead of inline strings

--- NodeMcuGarage/NodeMcuGarage.ino
+++ NodeMcuGarage/NodeMcuGarage.ino
@@ -3,6 +3,7 @@
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
+#include <FS.h>
 #include "WifiKeys.h"
 
 ESP8266WiFiMulti wifiMulti;
@@ -11,14 +12,8 @@ ESP8266WebServer server(80);
 const int pinPump = 5;
 const int pinLight = 4;
 
-void handleRoot();
-void handlePump();
-void handleLight();
-void handleNotFound();
-
-String styles = "<style>body{padding-top:3rem;display:grid;place-items:center;grid-gap:3rem;}form{height:8rem;width:50%;}button{width:100%;height:100%;font-size:3rem;border-radius:12px;}</style>";
-String formPump = "<form action=\"/pump\"><button type=\"submit\" formmethod=\"post\">Toggle Pump</button></form>";
-String formLight = "<form action=\"/light\"><button type=\"submit\" formmethod=\"post\">Toggle Light</button></form>";
+String getContentType(String filename); // convert the file extension to the MIME type
+bool handleFileRead(String path);       // send the right file to the client (if it exists)
 
 void setup(void) {
   Serial.begin(115200);
@@ -48,12 +43,14 @@ void setup(void) {
   } else {
     Serial.println("Error setting up MDNS responder!");
   }
+  
+  SPIFFS.begin();
 
-  server.on("/", HTTP_GET, handleRoot);
   server.on("/pump", HTTP_POST, handlePump);
   server.on("/light", HTTP_POST, handleLight);
-  server.onNotFound([](){
-    server.send(404, "text/plain", "404: Not found");
+  server.onNotFound([]() {                              // If the client requests any URI
+    if (!handleFileRead(server.uri()))                  // send it if it exists
+      server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
   });
 
   server.begin();
@@ -64,12 +61,26 @@ void loop(void) {
   server.handleClient();
 }
 
-void handleRoot() {
-  server.send(200, "text/html", getHTML());
+String getContentType(String filename) { // convert the file extension to the MIME type
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  return "text/plain";
 }
 
-String getHTML() {
-  return styles + "<body>" + formPump + formLight + "</body>";
+bool handleFileRead(String path) { // send the right file to the client (if it exists)
+  Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "index.html";         // If a folder is requested, send the index file
+  String contentType = getContentType(path);            // Get the MIME type
+  if (SPIFFS.exists(path)) {                            // If the file exists
+    File file = SPIFFS.open(path, "r");                 // Open it
+    size_t sent = server.streamFile(file, contentType); // And send it to the client
+    file.close();                                       // Then close the file again
+    return true;
+  }
+  Serial.println("\tFile Not Found");
+  return false;                                         // If the file doesn't exist, return false
 }
 
 void handlePump() {
