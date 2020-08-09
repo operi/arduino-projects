afec6991d7d7131fe89bc640c926e9f097c6a543
micaelamorena <mica.morena02@gmail.com>
2020-08-08 21:19:27 -0300

    lo lindo que quedo, wacho

--- NodeMcuGarage/NodeMcuGarage.ino
+++ NodeMcuGarage/NodeMcuGarage.ino
@@ -3,22 +3,19 @@
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
+#include <FS.h>
 #include "WifiKeys.h"
 
 ESP8266WiFiMulti wifiMulti;
+File fsUploadFile;
 ESP8266WebServer server(80);
 
-const int pinPump = D1;
-const int pinLight = D2;
+const int pinPump = 5;
+const int pinLight = 4;
 
-void handleRoot();
-void handlePump();
-void handleLight();
-void handleNotFound();
-
-String styles = "<style>body{padding-top:3rem;display:grid;place-items:center;grid-gap:3rem;}form{height:8rem;width:50%;}button{width:100%;height:100%;font-size:3rem;border-radius:12px;}</style>";
-String formPump = "<form action=\"/pump\"><button type=\"submit\" formmethod=\"post\">Toggle Pump</button></form>";
-String formLight = "<form action=\"/light\"><button type=\"submit\" formmethod=\"post\">Toggle Light</button></form>";
+String getContentType(String filename);
+bool handleFileRead(String path);
+void handleFileUpload();
 
 void setup(void) {
   Serial.begin(115200);
@@ -43,16 +40,29 @@ void setup(void) {
   Serial.println(WiFi.localIP());
 
   // Start the mDNS responder
-  if (MDNS.begin("arduino")) {
+  if (MDNS.begin("garage")) {
     Serial.println("mDNS responder started");
   } else {
     Serial.println("Error setting up MDNS responder!");
   }
+  
+  SPIFFS.begin();
 
-  server.on("/", HTTP_GET, handleRoot);
   server.on("/pump", HTTP_POST, handlePump);
   server.on("/light", HTTP_POST, handleLight);
-  server.onNotFound(handleNotFound);
+  server.on("/upload", HTTP_GET, []() {
+    if (!handleFileRead("/upload.html"))
+      server.send(404, "text/plain", "404: Not Found");
+  });
+
+  server.on("/upload", HTTP_POST,
+    [](){ server.send(200); },
+    handleFileUpload
+  );
+  server.onNotFound([]() {
+    if (!handleFileRead(server.uri()))
+      server.send(404, "text/plain", "404: Not Found");
+  });
 
   server.begin();
   Serial.println("HTTP server started");
@@ -60,14 +70,34 @@ void setup(void) {
 
 void loop(void) {
   server.handleClient();
+  MDNS.update();
 }
 
-void handleRoot() {
-  server.send(200, "text/html", getHTML());
+String getContentType(String filename){
+  if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
 }
 
-String getHTML() {
-  return styles + "<body>" + formPump + formLight + "</body>";
+bool handleFileRead(String path){
+  Serial.println("handleFileRead: " + path);
+  if(path.endsWith("/")) path += "index.html";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+    if(SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
+    Serial.println(String("\tSent file: ") + path);
+    return true;
+  }
+  Serial.println(String("\tFile Not Found: ") + path);
+  return false;
 }
 
 void handlePump() {
@@ -82,6 +112,25 @@ void handleLight() {
   server.send(303);
 }
 
-void handleNotFound() {
-  server.send(404, "text/plain", "404: Not found");
+void handleFileUpload(){
+  HTTPUpload& upload = server.upload();
+  if(upload.status == UPLOAD_FILE_START){
+    String filename = upload.filename;
+    if(!filename.startsWith("/")) filename = "/"+filename;
+    Serial.print("handleFileUpload Name: "); Serial.println(filename);
+    fsUploadFile = SPIFFS.open(filename, "w");
+    filename = String();
+  } else if(upload.status == UPLOAD_FILE_WRITE){
+    if(fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize);
+  } else if(upload.status == UPLOAD_FILE_END){
+    if(fsUploadFile) {
+      fsUploadFile.close();
+      Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
+      server.sendHeader("Location","/index.html");
+      server.send(303);
+    } else {
+      server.send(500, "text/plain", "500: couldn't create file");
+    }
+  }
 }
