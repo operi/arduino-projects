4dee9f57bb9755a2557ec5718df2cb8c20e1d709
Octavio <octaperi@gmail.com>
2020-08-30 00:14:03 -0300

    Add websocket handling code

--- NodeMcuGarage/data/app.js
+++ NodeMcuGarage/data/app.js
@@ -1,7 +1,18 @@
+const ws = new WebSocket('ws://192.168.0.200/');
+ws.onopen = function() {
+    console.log('WebSocket Client Connected');
+    ws.send("status");
+};
+ws.onmessage = function(e) {
+  console.log("Received: '" + e.data + "'");
+};
+
 function flipPump() {
     console.log("Flipping pump");
+    ws.send("pump");
 }
 
 function flipLight() {
     console.log("Flipping LIGHT");
+    ws.send("light");
 }
