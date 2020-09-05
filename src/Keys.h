15983cb5ce8f6c3695983bd82ab1c96a5e87af37
Octavio <octaperi@gmail.com>
2020-09-05 11:33:09 -0300

    Rename directory

---
+++ src/Keys.h
@@ -0,0 +1,12 @@
+#ifndef WIFIKEYS_H
+#define WIFIKEYS_H
+const char* ssid1 = "Fibertelwifi824 2.4GHz-Ext";
+const char* password1 = "141223240204";
+const char* ssid2 = "Fibertelwifi824 2.4GHz";
+const char* password2 = "141223240204";
+#endif
+
+#ifndef OTAKEYS_H
+#define OTAKEYS_H
+const char* OTA_password = "141223240204";
+#endif
